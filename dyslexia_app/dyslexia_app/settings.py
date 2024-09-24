f1cc813de414ceaf7eec2e8740a72a9e1ea08824
silvadaniell <djs@ic.ufal.br>
2024-09-24 17:47:10 -0300

    feat(users): crud de cadastro de usuário

--- dyslexia_app/dyslexia_app/settings.py
+++ dyslexia_app/dyslexia_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],  
+}
 
 
 # Internationalization
---
+++ dyslexia_app/users/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    

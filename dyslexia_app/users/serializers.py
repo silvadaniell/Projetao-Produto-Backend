c873388d1113e36de829f8d5d4b999858ba142a2
silvadaniell <djs@ic.ufal.br>
2024-09-25 21:13:10 -0300

    tirando espaço do código

--- dyslexia_app/users/serializers.py
+++ dyslexia_app/users/serializers.py
@@ -11,14 +11,12 @@ class UsersCadastroSerializer(serializers.ModelSerializer):
         }
         
     def update(self, instance, validated_data):
-    # Atualiza a instância com os dados validados
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.age = validated_data.get('age', instance.age)
 
-        # Atualiza a senha se fornecida
         password = validated_data.get('password')
         if password:
             instance.password = password 

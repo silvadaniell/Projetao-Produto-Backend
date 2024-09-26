19cd8b402ff1ea310e90f9b085eb6fecbcd7f78f
silvadaniell <djs@ic.ufal.br>
2024-09-25 21:11:30 -0300

    feat(serializers)Para garantir que a senha não seja exposta

--- dyslexia_app/users/models.py
+++ dyslexia_app/users/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
+def validate_age(value):
+    if value < 5:
+        raise ValidationError('A idade deve ser 5 ou maior que 5.')
+    
+    
 class Users(models.Model):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[validate_age])
 
     def __str__(self):
         return self.username
--- dyslexia_app/users/serializers.py
+++ dyslexia_app/users/serializers.py
@@ -5,3 +5,23 @@ class UsersCadastroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'age']
+        
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+        
+    def update(self, instance, validated_data):
+    # Atualiza a instância com os dados validados
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.age = validated_data.get('age', instance.age)
+
+        # Atualiza a senha se fornecida
+        password = validated_data.get('password')
+        if password:
+            instance.password = password 
+
+        instance.save()
+        return instance   

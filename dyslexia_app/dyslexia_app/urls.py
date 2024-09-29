8bc254da8a0d1cf7c075091b07fe21d3fd03455c
silvadaniell <djs@ic.ufal.br>
2024-09-29 17:25:46 -0300

    feat(Atividades):  adicinais

--- dyslexia_app/dyslexia_app/urls.py
+++ dyslexia_app/dyslexia_app/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('api/', include('atividades.urls')),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
